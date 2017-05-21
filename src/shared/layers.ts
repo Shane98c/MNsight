678ff36208ee0514789318b6574d879307139ba3
shane98c <shane98c@gmail.com>
2017-05-20 23:50:48 -0500

    http --> https

--- src/shared/layers.ts
+++ src/shared/layers.ts
@@ -1,12 +1,12 @@
 export const layers = {
     "mnLidar": {
       'type': 'arcgisraster',
-      "url":"http://arcgis.dnr.state.mn.us/arcgis/rest/services/elevation/mn_hillshade_web_mercator/MapServer?f=json",
+      "url":"https://arcgis.dnr.state.mn.us/arcgis/rest/services/elevation/mn_hillshade_web_mercator/MapServer?f=json",
       "tileSize": 256
     },
     "colorTopo": {
       'type': 'arcgisraster',
-      "url":"http://arcgis.dnr.state.mn.us/arcgis/rest/services/elevation/elevation_mn_1mDEM_cache/MapServer?f=json",
+      "url":"https://arcgis.dnr.state.mn.us/arcgis/rest/services/elevation/elevation_mn_1mDEM_cache/MapServer?f=json",
       "tileSize": 256
     }
   };
