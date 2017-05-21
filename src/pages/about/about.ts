998b8c9ba89b2b3b3dc1e70b16aae4bde8079803
shane98c <shane98c@gmail.com>
2017-05-20 23:31:42 -0500

    working static maps

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -12,12 +12,18 @@ import { LocService } from '../../shared/loc.service'
 export class AboutPage {
   public under: any;
   public curLoc: any;
+  public mapClip: string;
   constructor(private navCtrl: NavController, private navParams: NavParams, private loadingCtrl: LoadingController, private underService: UnderService, public locService: LocService) {
   }
 
   ionViewWillEnter(){
     if (this.navParams.data && this.navParams.data.under) {
       this.under = this.navParams.get('under');
+      let loc = {
+        lat: this.under.point['epsg:4326'][1],
+        lng: this.under.point['epsg:4326'][0]
+      }
+      this.buildMapClip(loc);
     } else {
       this.hereNow();
     }
@@ -25,6 +31,17 @@ export class AboutPage {
   ionViewWillLeave(){
     this.navParams.data = undefined;
     this.under = undefined;
+  }
+  //https://api.mapbox.com/styles/v1/mapbox/light-v9/static/pin-s(-93.26,44.98)/-94.76,44.98,10,0,0/800x325@2x?access_token=pk.eyJ1IjoiZmx5b3ZlcmNvdW50cnkiLCJhIjoiNDI2NzYzMmYxMzI5NWYxMDc0YTY5NzRiMzdlZDIyNTAifQ.x4T-qLEzRQMNFIdnkOkHKQ"
+  buildMapClip(loc):void {
+    this.mapClip = [
+      'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/pin-s(',
+      loc.lng,',',loc.lat,
+      ')/',
+      loc.lng,',',loc.lat,
+      ',10,0,0/800x325@2x?access_token=pk.eyJ1IjoiZmx5b3ZlcmNvdW50cnkiLCJhIjoiNDI2NzYzMmYxMzI5NWYxMDc0YTY5NzRiMzdlZDIyNTAifQ.x4T-qLEzRQMNFIdnkOkHKQ'
+    ].join('');
+    console.log(this.mapClip);
   }
   hereNow():void {
     let loading = this.loadingCtrl.create();
@@ -36,6 +53,7 @@ export class AboutPage {
             lat: res.coords.latitude,
             lng: res.coords.longitude
           };
+          this.buildMapClip(loc);
           this.underService.getUnder(loc)
           .then(UnderData => {
             this.under = UnderData;
