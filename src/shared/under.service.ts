874c7c914ba6fdc9d2773d08651899cd3235761e
Shane98c <shane98c@GMAIL.COM>
2022-11-28 17:39:30 +1300

    under service fix from a while ago

--- src/shared/under.service.ts
+++ src/shared/under.service.ts
@@ -1,39 +1,48 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
-import { soils } from './soils';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/toPromise";
+import { soils } from "./soils";
 
 @Injectable()
 export class UnderService {
-  private dnrURL = 'https://maps2.dnr.state.mn.us/cgi-bin/mapserv64?map=WUYH_MAPFILE&mode=nquery&qformat=json&mapxy=';
-  constructor(private http: Http) { }
+  private dnrURL =
+    "https://maps2.dnr.state.mn.us/cgi-bin/mapserv76?map=WUYH_MAPFILE&mode=nquery&qformat=json&mapxy=";
+  constructor(private http: Http) {}
 
-  getUnder(loc): Promise <any> {
-    const url = [this.dnrURL, loc.lng, '+', loc.lat].join('');
-    return this.http.get(url)
-     .toPromise()
-     .then(response => {
-       let formattedGeo = this.formatGeo(response.json().result);
-       return formattedGeo as any;
-     })
+  getUnder(loc): Promise<any> {
+    const url = [this.dnrURL, loc.lng, "+", loc.lat].join("");
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((response) => {
+        let formattedGeo = this.formatGeo(response.json().result);
+        return formattedGeo as any;
+      });
   }
 
   formatGeo(geo) {
-    if(geo.geology) {
+    if (geo.geology) {
       var geology = geo.geology;
       geology.soilName = soils[geology.soils];
-      var layers = ["mesozoic_cretaceous","mesozoic_jurassic","paleozoic_cambrian","paleozoic_devonian","paleozoic_ordovician","precambrian_archean", "precambrian_proterozoic"];
+      var layers = [
+        "mesozoic_cretaceous",
+        "mesozoic_jurassic",
+        "paleozoic_cambrian",
+        "paleozoic_devonian",
+        "paleozoic_ordovician",
+        "precambrian_archean",
+        "precambrian_proterozoic",
+      ];
       var i = 0;
       var layerArr = [];
       for (i = 0; i < layers.length; i++) {
-        if (geology.hasOwnProperty(layers[i])){
+        if (geology.hasOwnProperty(layers[i])) {
           var layerName = layers[i].replace("_", " ");
-          layerArr.push(geology[layers[i]] + ': (' +layerName+')');
+          layerArr.push(geology[layers[i]] + ": (" + layerName + ")");
           geology.geolLayerContent = layerArr;
         }
       }
     }
     return geo;
   }
-
 }
